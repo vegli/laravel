bc04b6f06fe6b7816c65d936fcc48a48e780ffff
Veljko Gligorijevic <gligorijevicveljko3@gmail.com>
2022-09-01 15:53:21 +0200

    Kreriane migracije, seederi i fabrike

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Customer::factory()
+         ->count(25)
+         ->hasInvoice(10)
+         ->create();
+        Customer::factory()
+         ->count(100)
+         ->hasInvoice(5)
+         ->create();
+        Customer::factory()
+         ->count(100)
+         ->hasInvoice(3)
+         ->create();
+        Customer::factory()
+         ->count(5)
+         ->hasInvoice()
+         ->create();
+    }
+}
